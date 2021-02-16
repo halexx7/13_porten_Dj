c805170a97b961ba7995c44bff519fa943bf102f
halexx7 <halexx7@gmail.com>
2021-02-16 14:11:58 +0300

    Django ORM start

--- watch_market/urls.py
+++ watch_market/urls.py
@@ -1,4 +1,6 @@
 import mainapp.views as mainapp
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -11,3 +13,8 @@ urlpatterns = [
     path('contacts/', mainapp.contacts, name='contacts'),
     path('admin/', admin.site.urls),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
